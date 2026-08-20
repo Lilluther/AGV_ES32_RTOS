a3a3ee470eb3156f2c220a761c59a288ebb08508
Joel Kibiwott <joelkibiwott99@gmail.com>
2026-08-20 07:36:15 +0300

    feat(app) Create operational state variables
    Implement an enum to define operational mode for the AGV
    Implement a struct to hold display status to the LCD

--- components/app_tasks/include/app_state.h
+++ components/app_tasks/include/app_state.h
@@ -1,4 +1,26 @@
 #ifndef APP_STATE_H
 #define APP_STATE_H
 
+//#include <stdint.h>
+#include <stdbool.h>
+
+/**
+ * @brief AGV operational modes.
+ */
+typedef enum {
+    AGV_MODE_OFF = 0,
+    AGV_MODE_MANUAL,
+    AGV_MODE_AUTO
+} agv_mode_t;
+
+/**
+ * @brief Status structure passed to the display task.
+ */
+typedef struct {
+    agv_mode_t current_mode;
+    float front_distance_cm;
+    bool line_detected;
+    char status_msg[32];
+} display_status_t;
+
 #endif
